fe04cc962909fa4726b91c844ded89b839cb9684
William Schroeder <wschroeder@gmail.com>
2020-05-27 11:25:20 -0500

    F12 hotkey for Deft

--- init.el
+++ init.el
@@ -131,4 +131,5 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 (global-set-key [f8] 'neotree-toggle)
+(global-set-key [f12] 'deft)
 

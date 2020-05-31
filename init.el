1bb434bd6c8fe6dad4c9185646cfcae86f8fef39
William Schroeder <wschroeder@gmail.com>
2020-05-31 16:51:13 -0500

    Moved deft configuration to its own file

--- init.el
+++ init.el
@@ -106,22 +106,7 @@
 (define-key ac-completing-map "\t" 'ac-complete) ; use tab to complete
 
 ;; Deft config
-(setq deft-default-extension "md")
-
-(defun new-log-for-today ()
-  (interactive)
-  (deft-filter-clear)
-  (let ((deft-default-extension "org")
-        (today-name (concat "TODO:" (format-time-string "%F"))))
-    (execute-kbd-macro (kbd today-name))
-    (if (length (deft-current-files))
-        (deft-complete)
-      (deft-new-file))))
-
-(defun setup-deft-mode ()
-  (local-set-key (kbd "C-c C-p") 'new-log-for-today))
-
-(add-hook 'deft-mode-hook 'setup-deft-mode)
+(load "~/.emacs.d/deft.el")
 
 ;; Helm-mode config
 (global-set-key (kbd "C-x C-n") 'helm-projectile)

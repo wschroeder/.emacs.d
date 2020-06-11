965f7d6f6a1eb4ad51aa138c40635e61ccd9021f
William Schroeder <wschroeder@gmail.com>
2020-06-11 14:24:08 -0500

    Use a custom sorting approach with Deft
    Deft presumably is intended to mimic Notational Velocity. One very nice feature of Notational Velocity was that it
    sorted your search in a human-friendly fashion. That is, if the title of a note matched what you searched for, it would
    come up first before all other kind of matches, which would be sorted by modified time. Deft, on the other hand, has an
    awkward "sort all titles alphabetically" vs "sort all files by modified time" approach. I wanted the middle ground, so I
    implemented a custom sort for the view. Note that Deft presorts all files in order to speed up the filter-and-display
    process, which is commendable; my sort happens after the search.

--- deft.el
+++ deft.el
@@ -89,5 +89,60 @@ it is on a headline, try all direct children. "
         (find-file (concat "~/.deft/" today-entry))
       (copy-and-find-deft-todos latest-entry today-entry))))
 
+(defun deft-first-filter-entry (filter)
+  (if (null filter)
+      ""
+    (first filter)))
+
+(defun deft-files-with-search-prefix-in-title (files)
+  "If the deft search is for \"foo\", return files whose titles start with \"foo\"."
+  (cl-remove-if-not #'(lambda (file)
+                        (string-prefix-p (deft-first-filter-entry deft-filter-regexp)
+                                         (deft-file-title file)))
+                    files))
+
+(defun deft-files-with-search-not-as-prefix-in-title (files)
+  "If the deft search is for \"foo\", return files whose titles
+include \"foo\" but do not start with \"foo\"."
+  (cl-remove-if-not #'(lambda (file)
+                        (let ((title (deft-file-title file)))
+                          (and
+                           (string-match-p (deft-first-filter-entry deft-filter-regexp)
+                                           title)
+                           (not (string-prefix-p (deft-first-filter-entry deft-filter-regexp)
+                                                 title)))))
+                    files))
+
+(defun deft-custom-sort (files)
+  (let* ((prefixed (deft-files-with-search-prefix-in-title files))
+         (included (deft-files-with-search-not-as-prefix-in-title files))
+         (therest  (set-difference (set-difference files
+                                                   prefixed
+                                                   :test 'equal)
+                                   included
+                                   :test 'equal)))
+    (append (deft-sort-files-by-title prefixed)
+            (deft-sort-files-by-title included)
+            therest)))
+
+(defun deft-filter-increment ()
+  "Append character to the filter regexp and update `deft-current-files'."
+  (interactive)
+  (let ((char last-command-event))
+    (if (= char ?\S-\ )
+        (setq char ?\s))
+    (setq char (char-to-string char))
+    (if (and deft-incremental-search (string= char " "))
+        (setq deft-filter-regexp (cons "" deft-filter-regexp))
+      (progn
+        (if (car deft-filter-regexp)
+            (setcar deft-filter-regexp (concat (car deft-filter-regexp) char))
+          (setq deft-filter-regexp (list char)))
+        (setq deft-current-files (deft-filter-files deft-current-files))
+        (setq deft-current-files (delq nil deft-current-files))
+        (setq deft-current-files (deft-custom-sort deft-current-files))
+        (deft-refresh-browser)
+        (run-hooks 'deft-filter-hook)))))
+
 (global-set-key (kbd "C-x C-l") 'find-today-deft-log)
 (global-set-key [f12] 'deft)

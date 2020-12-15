2bd42586e825a8f61b065ec7bc0ff786890a4356
William Schroeder <wschroeder@gmail.com>
2020-12-15 14:04:15 -0600

    Removed melpa-stable for now

--- packages.el
+++ packages.el
@@ -1,8 +1,8 @@
 (setq package-archives
       '(("ELPA" . "http://tromey.com/elpa/")
         ("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")
-        ("melpa-stable" . "http://stable.melpa.org/packages/")))
+        ("melpa" . "http://melpa.org/packages/")))
+        ;; ("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 (require 'package)
 
